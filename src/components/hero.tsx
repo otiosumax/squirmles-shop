aeb2c9c89fb60ebe8a6e07996933d06cf4aa7bda
PRPDSS <prpdss@gmail.com>
2026-06-25 01:43:08 +0300

    basic Hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,26 @@
-import '../styles/hero.css';
+import "../styles/hero.css";
 
 export default function Hero() {
   return (
-    <>
-     <p>Черви! Черви! Черви!</p>
-    </>
+    <div className="hero">
+      <h1>
+        Самые
+        <br />
+        крутые
+        <br />
+        черви!
+      </h1>
+      <div>Здесь будет картинка</div>
+      <div>
+        <h2>
+          Пушистые, цветастые и веселые. Все о них мечтают, но не все
+          признаются.
+        </h2>
+        <div className="shop-and-shipping">
+          <p className="shop-button">Закупиться</p>
+          <p>Бесплатная доставка при заказе от 1000$!</p>
+        </div>
+      </div>
+    </div>
   );
 }
